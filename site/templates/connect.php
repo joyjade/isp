29451250288bd1aad7ef07f2474385bc875cf891
Joy Jade <jadeandpearls@gmail.com>
2022-01-26 18:50:00 -0800

    mobile contact; photo;

--- site/templates/connect.php
+++ site/templates/connect.php
@@ -4,9 +4,9 @@
 <div class="main">
   <section class="split flush top">
     <?php snippet('connect', ['contact' => $page]) ?>
-    <div class="image forest">  
+    <div class="image moss">  
       <?php if($cover = $page->image()) :?>
-       <figure>
+       <figure class="collage">
          <img src="<?= $cover-> url() ?>" alt="">
        </figure>
       <?php endif; ?>
