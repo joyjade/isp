91d904941e8863233d6115529a14e6cf58a9c51a
JJ <jadeandpearls@gmail.com>
2022-06-01 03:04:08 -0700

    trying multisite;

--- index.php
+++ index.php
@@ -2,4 +2,28 @@
 
 require 'kirby/bootstrap.php';
 
+$sites = [
+  'inversesurveillance.com' => 'inversesurveillance.com'
+];
+
+$host = Url::host();
+$root = $sites[$host];
+$url  = 'http://' . $host;
+
+$kirby = new Kirby([
+  'roots' => [
+    'index'   => __DIR__,
+    'site'    => $root . '/site',
+    'content' => $root . '/content',
+    'media'   => $root . '/media',
+    'assets'  => $root . '/assets'
+  ],
+  'urls' => [
+    'media'  => $url . '/' . $root . '/media',
+    'assets' => $url . '/' . $root . '/assets',
+  ],
+]);
+
+echo $kirby->render();
+
 echo (new Kirby)->render();
